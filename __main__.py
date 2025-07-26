d2ead75a9fc5ec0d6f4372b893b01879a914a8c3
jvondie <jvondersmith@protonmail.com>
2025-07-26 09:26:23 -0400

    DE-1 change school

--- __main__.py
+++ __main__.py
@@ -6,7 +6,7 @@ from pulumi_aws import s3
 # Create an AWS resource (S3 Bucket)
 bucket = s3.Bucket('vcu-bucket-072625')
 
-next_bucket = s3.Bucket('bucket-vcu-2507')
+next_bucket = s3.Bucket('bucket-umd-2507')
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
