f78bc56c859aded8ab4222ab1b422b86782a581c
jvondie <jvondersmith@protonmail.com>
2025-07-26 09:18:20 -0400

    DE-1 add files

---
+++ __main__.py
@@ -0,0 +1,12 @@
+"""An AWS Python Pulumi program"""
+
+import pulumi
+from pulumi_aws import s3
+
+# Create an AWS resource (S3 Bucket)
+bucket = s3.Bucket('vcu-bucket-072625')
+
+next_bucket = s3.Bucket('bucket-vcu-2507')
+
+# Export the name of the bucket
+pulumi.export('bucket_name', bucket.id)
